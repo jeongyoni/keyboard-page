8704e03805a5c92b2f5137477eba19501b2e80da
jeongyoni <jungyoni99@gmail.com>
2025-03-21 17:43:47 +0900

    win 수정
    아직도 소리끊김 발생

--- keyboard-script-win.js
+++ keyboard-script-win.js
@@ -51,9 +51,9 @@ function getMilkySound(key) {
   if (r2Keys.includes(key))       return milkySounds.r2;
   if (r1Keys.includes(key))       return milkySounds.r1;
   if (controlKeys.includes(key))  return milkySounds.control;
-  if (key === " ")        return milkySounds.space;
-  if (key === "Enter")    return milkySounds.enter;
-  if (key === "Backspace")return milkySounds.backspace;
+  if (key === " ")                return milkySounds.space;
+  if (key === "Enter")            return milkySounds.enter;
+  if (key === "Backspace")        return milkySounds.backspace;
   return null;
 }
 
@@ -67,47 +67,12 @@ function playCachedSound(url) {
   }
 }
 
-function isHangulSyllable(char) {
-  const code = char.charCodeAt(0);
-  return code >= 0xAC00 && code <= 0xD7A3;
-}
-
-function decomposeHangul(syllable) {
-  const SBase = 0xAC00, LBase = 0x1100, VBase = 0x1161, TBase = 0x11A7;
-  const LCount = 19, VCount = 21, TCount = 28;
-  const SIndex = syllable.charCodeAt(0) - SBase;
-  const LIndex = Math.floor(SIndex / (VCount * TCount));
-  const VIndex = Math.floor((SIndex % (VCount * TCount)) / TCount);
-  const TIndex = SIndex % TCount;
-  const initials = ["ㄱ","ㄲ","ㄴ","ㄷ","ㄸ","ㄹ","ㅁ","ㅂ","ㅃ","ㅅ","ㅆ","ㅇ","ㅈ","ㅉ","ㅊ","ㅋ","ㅌ","ㅍ","ㅎ"];
-  const medials = ["ㅏ","ㅐ","ㅑ","ㅒ","ㅓ","ㅔ","ㅕ","ㅖ","ㅗ","ㅘ","ㅙ","ㅚ","ㅛ","ㅜ","ㅝ","ㅞ","ㅟ","ㅠ","ㅡ","ㅢ","ㅣ"];
-  const finals = ["", "ㄱ","ㄲ","ㄳ","ㄴ","ㄵ","ㄶ","ㄷ","ㄹ","ㄺ","ㄻ","ㄼ","ㄽ","ㄾ","ㄿ","ㅀ","ㅁ","ㅂ","ㅄ","ㅅ","ㅆ","ㅇ","ㅈ","ㅊ","ㅋ","ㅌ","ㅍ","ㅎ"];
-  const initial = initials[LIndex];
-  const medial = medials[VIndex];
-  const final = finals[TIndex];
-  return { initial, medial, final };
-}
-
 document.addEventListener("keydown", function(event) {
-  if (event.isComposing) return;
+  // Windows에서는 IME 조합 여부 무시
   const pressedKey = event.key;
-  if (isHangulSyllable(pressedKey)) {
-    const decomposed = decomposeHangul(pressedKey);
-    if (decomposed.initial) {
-      const soundFile = getMilkySound(decomposed.initial);
-      if (soundFile) playCachedSound(soundFile);
-    }
-    if (decomposed.medial) {
-      const soundFile = getMilkySound(decomposed.medial);
-      if (soundFile) playCachedSound(soundFile);
-    }
-    if (decomposed.final && decomposed.final !== "") {
-      const soundFile = getMilkySound(decomposed.final);
-      if (soundFile) playCachedSound(soundFile);
-    }
-  } else {
-    const soundFile = getMilkySound(pressedKey);
-    if (soundFile) playCachedSound(soundFile);
+  const soundFile = getMilkySound(pressedKey);
+  if (soundFile) {
+    playCachedSound(soundFile);
   }
 });
 
