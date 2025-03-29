b36055eb45bcfd9d19c38b0879726cefd289b102
jeongyoni <jungyoni990604@gmail.com>
2025-03-30 01:29:48 +0900

    다시 수정

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // ← Home.jsx로 연결
-import App from './App.js';
+import App from './App';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
