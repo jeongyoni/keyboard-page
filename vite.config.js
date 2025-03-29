75d562e46b9e1f2dc2d39886760e79f9fc8e802e
jeongyoni <jungyoni990604@gmail.com>
2025-03-30 02:58:53 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({ 
-  plugins: [react()],
-});
+export default {
+  esbuild: {
+    loader: {
+      '.js': 'jsx', // .js 파일을 jsx로 처리하도록 설정
+    }
+  },
+};
